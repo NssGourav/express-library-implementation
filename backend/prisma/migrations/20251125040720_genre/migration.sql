a8a60a427fc4e56c8f37c761af84d18f90ffec35
NssGourav <gourav341111@gmail.com>
2025-11-25 09:42:23 +0530

    added genre int id as it is not matching with the schema i'm doubtfull

---
+++ backend/prisma/migrations/20251125040720_genre/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_BookToGenre` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `genreId` to the `Book` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE `_BookToGenre` DROP FOREIGN KEY `_BookToGenre_A_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `_BookToGenre` DROP FOREIGN KEY `_BookToGenre_B_fkey`;
+
+-- AlterTable
+ALTER TABLE `Book` ADD COLUMN `genreId` INTEGER NOT NULL;
+
+-- DropTable
+DROP TABLE `_BookToGenre`;
+
+-- AddForeignKey
+ALTER TABLE `Book` ADD CONSTRAINT `Book_genreId_fkey` FOREIGN KEY (`genreId`) REFERENCES `Genre`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
